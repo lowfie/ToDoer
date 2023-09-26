a6a9c1596dbb199412838c118a65974d3fe201e6
Andrew Veselov <vesel41223@gmail.com>
2023-09-26 15:29:16 +0300

    add static files

--- src/urls.py
+++ src/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -10,32 +11,35 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
+from src import settings
 from config import config
 
-
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 API_VERSION = config.api.version
 
 urlpatterns = [
-   path(f'{API_VERSION}admin/', admin.site.urls),
-   path(f'{API_VERSION}docs/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path(f'{API_VERSION}admin/', admin.site.urls),
+    path(f'{API_VERSION}docs/', schema_view.with_ui('swagger', cache_timeout=0)),
 
-   path(f'{API_VERSION}token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-   path(f'{API_VERSION}token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-   path(f'{API_VERSION}token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path(f'{API_VERSION}token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(f'{API_VERSION}token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(f'{API_VERSION}token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-   path(f'{API_VERSION}task/', include('src.tasks.urls')),
-   path(f'{API_VERSION}user/', include('src.users.urls')),
+    path(f'{API_VERSION}task/', include('src.tasks.urls')),
+    path(f'{API_VERSION}user/', include('src.users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()

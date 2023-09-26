c92596c60256e41b145080c0768e520427d0d77c
Andrew Veselov <vesel41223@gmail.com>
2023-09-26 18:45:53 +0300

    add name for endpoint

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from src.tasks.views import get_task_details, create_task, update_task, ExploreTask
 
 urlpatterns = [
-    path('create/', create_task),
-    path('<int:id>/', get_task_details),
-    path('<int:id>/update/', update_task),
-    path('explore/', ExploreTask.as_view())
+    path('create/', create_task, name='create_task'),
+    path('<int:id>/', get_task_details, name='get_task_details'),
+    path('<int:id>/update/', update_task, name='update_task'),
+    path('explore/', ExploreTask.as_view(), name='explore_task')
 ]
